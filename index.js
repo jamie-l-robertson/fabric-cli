b582870d554241c02c0ecb3c594a4dc9b4d57297
Jamie Robertson <jamie.robertson@cellosignal.com>
2018-07-19 21:49:07 +0100

    Updated tool to use inquirer, now selectable options and stores previously added builds

--- index.js
+++ index.js
@@ -2,63 +2,95 @@
 
 'use strict';
 
-const program = require('commander');
-const exec = require('child_process').exec;
+const inquirer = require('inquirer');
 const appRoot = require('app-root-path');
-const pkg = require('./package.json');
-const buildConfig = require(appRoot + '/config.json');
-const cmd = 'grunt';
 const fs = require('fs');
+const figlet = require('figlet');
+const chalk = require('chalk');
+const exec = require('child_process').exec;
+const prompt = inquirer.createPromptModule();
+const buildConfig = require(appRoot + '/config.json');
 
 function log(data) {
-  data.stdout.on('data', function (data) {
-    process.stdout.write(data);
-  });
-
-  data.stderr.on('data', function (data) {
-    process.stdout.write(data);
+  data.stdout.on('data', data => process.stdout.write(data));
+  data.stderr.on('data', data => process.stdout.write(data));
+  data.on('close', function(code) {
+    console.log('[END] code', code);
+    console.log('[END] stdout "%s"', stdout);
+    console.log('[END] stderr "%s"', stderr);
   });
 }
 
-function argument(task, path) {
-  var command;
-  task === 'start' ? command = cmd : command = cmd + ' ' +  task;
+function defaultTask(path) {
+  return 'rm src/build; ln -s ' + path + ' src/build; grunt';
+}
 
-  if(task === 'connect' && path != undefined) {
-    let cwd = process.cwd();
-    let buildPath = cwd  + '/src/build/';
-    let buildInfo;
+function argument(task, name, path) {
+  const config = readConfig();
+  let project;
+  let command;
+  let buildInfo;
 
-    command = 'rm src/build; ln -s ' + path + ' src/build';
-    exec(command);
+  if (task === 'connect') {
+    command = 'rm src/build; ln -s ' + path + ' src/build; grunt';
+    buildInfo = require(path + 'package.json');
+    buildConfig.projects.push({ name: buildInfo.name, path: path });
 
-    buildInfo = require(path + 'src/build/package.json');
-    buildConfig.projects.push({'name': buildInfo.name, 'path': path});
-
-    fs.writeFile(appRoot + '/config.json', JSON.stringify(buildConfig, null, 2), function (err) {
+    fs.writeFile(appRoot + '/config.json', JSON.stringify(buildConfig, null, 2), err => {
       if (err) return console.log(err);
-      console.log('build added');
+      console.log('Build stored!');
     });
+  }
 
+  if (task === 'start') {
+    project = config.projects.find(obj => obj.name === name);
+    command = defaultTask(project.path);
   }
 
   return command;
 }
 
-function run(task, path) {
-  let command = argument(task, path);
-  let fabric = exec(command);
+function readConfig() {
+  return JSON.parse(fs.readFileSync(appRoot + '/config.json', 'utf8'));
+}
+
+function getBuilds() {
+  const builds = readConfig();
+  const options = builds.projects.map(proj => proj.name);
+  return options;
+}
+
+function run(task, name, path) {
+  const command = argument(task, name, path);
+  const fabric = exec(command);
   log(fabric);
 }
 
-program
-  .version(pkg.version)
-  .arguments('[task] [path]')
-  .version(pkg.version)
-  .description('CLI for Fabric')
-  .action(function (task, path) {
-     run(task, path);
-  });
+const questions = [
+  {
+    type: 'list',
+    name: 'command',
+    message: 'What would you like to do?',
+    choices: ['start', 'connect'],
+  },
+  {
+    type: 'list',
+    name: 'build',
+    message: 'Which build would you like to start?',
+    choices: getBuilds(),
+    when: answers => answers.command === 'start',
+  },
+  {
+    type: 'input',
+    name: 'path',
+    message: 'Enter the build path',
+    when: answers => answers.command === 'connect',
+  },
+];
 
-program.parse(process.argv);
-if (program.args.length === 0) program.help();
+console.log(chalk.yellow(figlet.textSync('Fabric', { horizontalLayout: 'full' })));
+
+prompt(questions).then(answers => {
+  const { command, build, path } = answers;
+  run(command, build, path);
+});
