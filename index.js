182dd4241eebcc2a9ad49137a8345e95f08e1ac7
Jamie Robertson <jamie.robertson@cellosignal.com>
2019-04-11 16:37:27 +0100

    Updated to work anywhere, added abiltiy to set fabric root path

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const exec = require('child_process').exec;
 const prompt = inquirer.createPromptModule();
 const buildConfig = require(appRoot + '/config.json');
 
+const base_cmd = 'Connect base';
+const fabric_base_cmd = 'Connect Fabric';
+const connect_cmd = 'Add build';
+const start_cmd = 'Start build';
+
+
 function trailingSlash(path) {
   return path.endsWith('/') ? path : path + '/';
 }
@@ -18,42 +24,53 @@ function trailingSlash(path) {
 function log(data) {
   data.stdout.on('data', data => process.stdout.write(data));
   data.stderr.on('data', data => process.stdout.write(data));
-  data.on('close', function (code) {
+  data.on('close', function (code, stdout, stderr) {
     if (code) console.log('[END] code', code);
     if (stdout) console.log('[END] stdout "%s"', stdout);
     if (stderr) console.log('[END] stderr "%s"', stderr);
   });
 }
 
-function defaultTask(path) {
-  return 'rm src/build; ln -s ' + path + ' src/build; grunt';
+function getFabric() {
+  return 'cd ' + trailingSlash(buildConfig.fabricPath);
 }
 
-function argument(task, name, path, basePath) {
+function defaultTask(path) {
+  const fabric = getFabric();
+  return fabric + '; rm src/build; ln -s ' + path + ' src/build; grunt';
+}
+
+function argument(task, name, path, fabricPath, basePath) {
   const config = readConfig();
+  const fabric = getFabric();
   let project;
   let command;
-  let buildInfo;
 
-  if (task === 'Connect build') {
-
-    command = 'rm src/build; ln -s ' + config.basePath + path + ' src/build; grunt';
-    buildInfo = require(config.basePath + trailingSlash(path) + 'package.json');
-    buildConfig.projects.push({ name: buildInfo.name, path: config.basePath + path });
-
+  if (task === connect_cmd) {
+    command = fabric + '; rm src/build; ln -s ' + config.basePath + path + ' src/build; grunt';
+    buildConfig.projects.push({ name: path, path: trailingSlash(config.basePath + path) });
     fs.writeFile(appRoot + '/config.json', JSON.stringify(buildConfig, null, 2), err => {
       if (err) return console.log(err);
       console.log('Build stored!');
     });
   }
 
-  if (task === 'Start build') {
+  if (task === start_cmd) {
     project = config.projects.find(obj => obj.name === name);
     command = defaultTask(project.path);
 
   }
 
-  if (task === 'Connect base') {
+  if (task === fabric_base_cmd) {
+    buildConfig.fabricPath = trailingSlash(fabricPath);
+
+    fs.writeFile(appRoot + '/config.json', JSON.stringify(buildConfig, null, 2), err => {
+      if (err) return console.log(err);
+      console.log('Base path stored!');
+    });
+  }
+
+  if (task === base_cmd) {
     buildConfig.basePath = trailingSlash(basePath);
 
     fs.writeFile(appRoot + '/config.json', JSON.stringify(buildConfig, null, 2), err => {
@@ -79,8 +96,8 @@ function getBuilds() {
   return projects;
 }
 
-function run(task, name, path, rootPath) {
-  const command = argument(task, name, path, rootPath);
+function run(task, name, path, fabricPath, basePath) {
+  const command = argument(task, name, path, fabricPath, basePath);
   const fabric = command ? exec(command) : null;
   fabric !== null ? log(fabric) : null;
 }
@@ -90,32 +107,38 @@ const questions = [
     type: 'list',
     name: 'command',
     message: 'What would you like to do?',
-    choices: ['Start build', 'Connect build', 'Connect base'],
+    choices: [start_cmd, connect_cmd, fabric_base_cmd, base_cmd],
   },
   {
     type: 'list',
     name: 'build',
     message: 'Which build would you like to start?',
     choices: getBuilds(),
-    when: answers => answers.command === 'Start build',
+    when: answers => answers.command === start_cmd,
   },
   {
     type: 'input',
     name: 'path',
     message: 'Enter the build path',
-    when: answers => answers.command === 'Connect build',
+    when: answers => answers.command === connect_cmd,
+  },
+  {
+    type: 'input',
+    name: 'fabricPath',
+    message: 'Enter your fabric path',
+    when: answers => answers.command === fabric_base_cmd
   },
   {
     type: 'input',
     name: 'basePath',
     message: 'Enter your base path',
-    when: answers => answers.command === 'Connect base'
+    when: answers => answers.command === base_cmd
   },
 ];
 
 console.log(chalk.yellow(figlet.textSync('Fabric', { horizontalLayout: 'full' })));
 
 prompt(questions).then(answers => {
-  const { command, build, path, basePath } = answers;
-  run(command, build, path, basePath);
+  const { command, build, path, fabricPath, basePath } = answers;
+  run(command, build, path, fabricPath, basePath);
 });
