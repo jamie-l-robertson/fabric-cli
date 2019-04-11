5d4c4fe874de8cfaa98529f57083103fadb9bc40
Jamie Robertson <jamie.robertson@cellosignal.com>
2019-04-11 13:57:41 +0100

    Added basePath command, fixed a few errors

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ const exec = require('child_process').exec;
 const prompt = inquirer.createPromptModule();
 const buildConfig = require(appRoot + '/config.json');
 
+function trailingSlash(path) {
+  return path.endsWith('/') ? path : path + '/';
+}
+
 function log(data) {
   data.stdout.on('data', data => process.stdout.write(data));
   data.stderr.on('data', data => process.stdout.write(data));
-  data.on('close', function(code) {
-    console.log('[END] code', code);
-    console.log('[END] stdout "%s"', stdout);
-    console.log('[END] stderr "%s"', stderr);
+  data.on('close', function (code) {
+    if (code) console.log('[END] code', code);
+    if (stdout) console.log('[END] stdout "%s"', stdout);
+    if (stderr) console.log('[END] stderr "%s"', stderr);
   });
 }
 
@@ -25,16 +29,17 @@ function defaultTask(path) {
   return 'rm src/build; ln -s ' + path + ' src/build; grunt';
 }
 
-function argument(task, name, path) {
+function argument(task, name, path, basePath) {
   const config = readConfig();
   let project;
   let command;
   let buildInfo;
 
-  if (task === 'connect') {
-    command = 'rm src/build; ln -s ' + path + ' src/build; grunt';
-    buildInfo = require(path + 'package.json');
-    buildConfig.projects.push({ name: buildInfo.name, path: path });
+  if (task === 'Connect build') {
+
+    command = 'rm src/build; ln -s ' + config.basePath + path + ' src/build; grunt';
+    buildInfo = require(config.basePath + trailingSlash(path) + 'package.json');
+    buildConfig.projects.push({ name: buildInfo.name, path: config.basePath + path });
 
     fs.writeFile(appRoot + '/config.json', JSON.stringify(buildConfig, null, 2), err => {
       if (err) return console.log(err);
@@ -42,12 +47,23 @@ function argument(task, name, path) {
     });
   }
 
-  if (task === 'start') {
+  if (task === 'Start build') {
     project = config.projects.find(obj => obj.name === name);
     command = defaultTask(project.path);
+
+  }
+
+  if (task === 'Connect base') {
+    buildConfig.basePath = trailingSlash(basePath);
+
+    fs.writeFile(appRoot + '/config.json', JSON.stringify(buildConfig, null, 2), err => {
+      if (err) return console.log(err);
+      console.log('Base path stored!');
+    });
   }
 
   return command;
+
 }
 
 function readConfig() {
@@ -56,14 +72,17 @@ function readConfig() {
 
 function getBuilds() {
   const builds = readConfig();
-  const options = builds.projects.map(proj => proj.name);
-  return options;
+  let projects = new Array();
+
+  projects = Array.from(new Set(builds.projects.map(JSON.stringify))).map(JSON.parse);
+
+  return projects;
 }
 
-function run(task, name, path) {
-  const command = argument(task, name, path);
-  const fabric = exec(command);
-  log(fabric);
+function run(task, name, path, rootPath) {
+  const command = argument(task, name, path, rootPath);
+  const fabric = command ? exec(command) : null;
+  fabric !== null ? log(fabric) : null;
 }
 
 const questions = [
@@ -71,26 +90,32 @@ const questions = [
     type: 'list',
     name: 'command',
     message: 'What would you like to do?',
-    choices: ['start', 'connect'],
+    choices: ['Start build', 'Connect build', 'Connect base'],
   },
   {
     type: 'list',
     name: 'build',
     message: 'Which build would you like to start?',
     choices: getBuilds(),
-    when: answers => answers.command === 'start',
+    when: answers => answers.command === 'Start build',
   },
   {
     type: 'input',
     name: 'path',
     message: 'Enter the build path',
-    when: answers => answers.command === 'connect',
+    when: answers => answers.command === 'Connect build',
+  },
+  {
+    type: 'input',
+    name: 'basePath',
+    message: 'Enter your base path',
+    when: answers => answers.command === 'Connect base'
   },
 ];
 
 console.log(chalk.yellow(figlet.textSync('Fabric', { horizontalLayout: 'full' })));
 
 prompt(questions).then(answers => {
-  const { command, build, path } = answers;
-  run(command, build, path);
+  const { command, build, path, basePath } = answers;
+  run(command, build, path, basePath);
 });
